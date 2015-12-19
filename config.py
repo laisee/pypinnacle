dfa9b5548a6698f8956e4f0f2fbc3ca8d0cfbd3e
G Costin <tinwald@gmail.com>
2015-12-19 18:19:19 +0800

    added clean config file

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+config.py
+"""
+
+base_url = 'https://api.pinnaclesports.com/v1/'
+API_TOKEN = 'R0M4MDk0NzQ6TGFpc2VlODgmJg88'
+SLEEPTIME = 60 # seconds
+username=None
+pswd=None
